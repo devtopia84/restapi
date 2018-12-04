0261ac5a294a40530bc47dd7dbb73ddbe49a75ef
Aiden <hello@devtopia.net>
2018-12-04 22:52:45 +0900

    입력값 및 응답 확인

--- src/main/java/net/devtopia/rest/events/EventController.java
+++ src/main/java/net/devtopia/rest/events/EventController.java
@@ -1,5 +1,6 @@
 package net.devtopia.rest.events;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,11 +17,14 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
 
+    @Autowired
+    EventRepository eventRepository;
+
     @PostMapping
     public ResponseEntity createEvent(@RequestBody Event event) {
-        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        Event newEvent = eventRepository.save(event);
 
-        event.setId(10);
+        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
 
         return ResponseEntity.created(createdUri).body(event);
     }
---
+++ src/main/java/net/devtopia/rest/events/EventRepository.java
@@ -0,0 +1,6 @@
+package net.devtopia.rest.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
