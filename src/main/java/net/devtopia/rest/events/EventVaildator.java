cce07ebf6606e56a7865d8af82babb0caf162624
Aiden <hello@devtopia.net>
2018-12-13 00:52:48 +0900

    Bad Request 응답 14:02

--- src/main/java/net/devtopia/rest/events/EventVaildator.java
+++ src/main/java/net/devtopia/rest/events/EventVaildator.java
@@ -12,6 +12,7 @@ public class EventVaildator {
         if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
             errors.rejectValue("basePrice", "wrongValue", "기본값이 문제가 있습니다.");
             errors.rejectValue("maxPrice", "wrongValue", "최대값이 문제가 있습니다.");
+            errors.reject("wrongPrices", "Values of price are wrong.");
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
