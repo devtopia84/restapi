6fa9179c114d2b85b98dbf7ef9093c5e9e183002
Aiden <hello@devtopia.net>
2018-12-12 23:31:21 +0900

    Add test description annotations

--- src/test/java/net/devtopia/rest/events/EventControllerTests.java
+++ src/test/java/net/devtopia/rest/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package net.devtopia.rest.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.devtopia.rest.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,6 +35,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Inflearn Spring REST API")
@@ -65,6 +67,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 업는 값을 사용한 경우 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(10)
@@ -94,6 +97,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어 있는 경우 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto event = EventDto.builder()
                 .build();
@@ -108,6 +112,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못 경우 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Inflearn Spring REST API")
